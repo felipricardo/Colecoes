ea755fa3ca4ae997fa601c446684a396d3d043b4
felipricardo <felipebarbosaok@gmail.com>
2023-08-31 16:19:31 -0300

    up

--- Colecoes/Form1.cs
+++ Colecoes/Form1.cs
@@ -19,10 +19,13 @@ namespace Colecoes
 
         private void btnList_Click(object sender, EventArgs e)
         {
+            // Array
             string[] nomes = new string[3];
             nomes[0] = "Felipe";
             nomes[1] = "Gabriel";
             nomes[2] = "Toni";
+
+            List<string> nomes2 = new List<string>();
 
         }
     }
